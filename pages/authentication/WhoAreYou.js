c1ff7bbf64c70b9fb21291636e15540c392576db
Jasmine Mai <33132825+jasminemai97@users.noreply.github.com>
2019-02-22 11:02:02 -0800

    Added image buttons

--- pages/authentication/WhoAreYou.js
+++ pages/authentication/WhoAreYou.js
@@ -3,18 +3,38 @@ import {
   View,
   Text,
   StyleSheet,
+  TouchableOpacity,
+  Image,
   Button
 } from 'react-native';
 
 export default class WhoAreYou extends Component {
-
+  static navigationOptions = {
+    headerStyle: {
+      borderBottomWidth: 0,
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Are you a</Text>
         <Text style={styles.subtitle}>Customer or a Business Owner?</Text>
-        <Button title="Customer" onPress={() => this.props.navigation.navigate('C_Signup')} />
-        <Button title="Business Owner" onPress={() => this.props.navigation.navigate('B_Signup')} />
+        <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={() => this.props.navigation.navigate('C_Signup')}>
+          <Image
+            source={require('./customer.jpg')}
+            style={styles.ImageIconStyle}
+          />
+          <View style={styles.SeparatorLine} />
+          <Text style={styles.TextStyle}> Customer </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={() => this.props.navigation.navigate('B_Signup')}>
+          <Image
+            source={require('./business.jpg')}
+            style={styles.ImageIconStyle}
+          />
+          <View style={styles.SeparatorLine} />
+          <Text style={styles.TextStyle}> Business Owner </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -23,14 +43,33 @@ export default class WhoAreYou extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 30
   },
   title: {
     fontSize: 25,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   subtitle: {
     fontSize: 15,
+    paddingBottom: 15,
+  },
+  TextStyle: {
+    textAlign: 'center',
+    paddingBottom: 20
+  },
+  button: {
+    marginTop: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#E7E8EC'
+  },
+  ImageIconStyle: {
+    padding: 10,
+    margin: 10,
+    height: 150,
+    width: 300,
+    resizeMode: 'contain',
   }
 });
