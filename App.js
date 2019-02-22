c9672ab9cc102bbb5beae40e26217ce74210b191
Jasmine Mai <33132825+jasminemai97@users.noreply.github.com>
2019-02-21 22:06:29 -0800

    Added authentication screens

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+// Auth Screens
 import GetStarted from './authentication/GetStarted'
 import WhoAreYou from './authentication/WhoAreYou'
+import C_Signup from './authentication/C_Signup'
+import B_Signup from './authentication/B_Signup'
+import Signin from './authentication/Signin'
 
 export const Auth = createAppContainer(
   createStackNavigator({
     GetStarted: { screen: GetStarted },
     WhoAreYou: { screen: WhoAreYou },
-  }),
+    C_Signup: { screen: C_Signup },
+    B_Signup: { screen: B_Signup },
+    Signin: { screen: Signin },
+  }, {
+      navigationOptions: {
+        header: null
+      }
+    }
+  ),
 )
 
 type Props = {};
