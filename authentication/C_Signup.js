3a3ddcbf9ec1c5274ec930a1c424b5ce5e3062a3
Jasmine Mai <33132825+jasminemai97@users.noreply.github.com>
2019-02-21 22:06:58 -0800

    Customer Signup

---
+++ authentication/C_Signup.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class WhoAreYou extends Component {
+  render() {
+    return (
+      <View style={styles.container} >
+        <Image style={styles.image} source={require('./Logo.jpg')} />
+        <TextInput style={styles.input}
+          placeholder="Required"
+        />
+        <TextInput style={styles.input}
+          placeholder="Required"
+        />
+        <TextInput style={styles.input}
+          placeholder="Required"
+        />
+        <TextInput style={styles.input}
+          placeholder="Required"
+        />
+        <TextInput style={styles.input} secureTextEntry={true}
+          placeholder="Minimum 8 Characters"
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: 'white' }}>Sign Up</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: 'white' }}>Continue with Facebook</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: 'white' }}>Continue with Google</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: '85%',
+    textAlign: 'left',
+    borderBottomWidth: 1
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D33B32',
+    borderRadius: 10,
+    marginTop: 10,
+    padding: 10,
+    width: '85%',
+  },
+  image: {
+    height: 120,
+    resizeMode: 'contain',
+  },
+});
