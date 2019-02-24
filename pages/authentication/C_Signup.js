6e15ff3aaf8aca42029c35bc03588260f2f2bc97
Jasmine Mai <33132825+jasminemai97@users.noreply.github.com>
2019-02-24 15:52:40 -0800

    Cusomter Form Filled Validation

--- pages/authentication/C_Signup.js
+++ pages/authentication/C_Signup.js
@@ -14,28 +14,77 @@ export default class WhoAreYou extends Component {
       borderBottomWidth: 0,
     }
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      f_name: '',
+      l_name: '',
+      email: '',
+      phone: '',
+      password: '',
+    }
+  }
+
+  validate = () => {
+    const { f_name, l_name, email, phone, password } = this.state;
+    if (f_name == "") {
+      alert('Please fill in your first name.')
+    } else if (l_name == "") {
+      alert('Please fill in your last name.')
+    } else if (email == "") {
+      alert('Please fill in your email.')
+    } else if (phone == "") {
+      alert('Please fill in your phone.')
+    } else if (password == "") {
+      alert('Please fill in your password.')
+    }
+  }
+
   render() {
     return (
       <View style={styles.container} >
         <Image style={styles.image} source={require('./AuthResources/Logo.jpg')} />
+        {/* Inputs */}
         <TextInput style={styles.input}
           placeholder="First Name"
+          onChangeText={
+            f_name => this.setState({ f_name })
+          }
         />
         <TextInput style={styles.input}
           placeholder="Last Name"
+          onChangeText={
+            l_name => this.setState({ l_name })
+          }
         />
         <TextInput style={styles.input}
           placeholder="Email"
+          onChangeText={
+            email => this.setState({ email })
+          }
         />
         <TextInput style={styles.input}
           placeholder="Phone"
+          onChangeText={
+            phone => this.setState({ phone })
+          }
         />
         <TextInput style={styles.input} secureTextEntry={true}
           placeholder="Password"
+          onChangeText={
+            password => this.setState({ password })
+          }
         />
-        <TouchableOpacity style={styles.button}>
+
+        {/* Sign Up */}
+        <TouchableOpacity onPress={this.validate}
+          style={styles.button}
+        >
           <Text style={{ color: 'white' }}>Sign Up</Text>
         </TouchableOpacity>
+
+        {/* Sign up with FB */}
         <TouchableOpacity style={styles.whiteButton}>
           <Image
             source={require('./AuthResources/facebooklogo.png')}
@@ -43,6 +92,8 @@ export default class WhoAreYou extends Component {
           />
           <Text style={{ color: '#D33B32' }}>Continue with Facebook</Text>
         </TouchableOpacity>
+
+        {/* Sign up with G+ */}
         <TouchableOpacity style={styles.whiteButton}>
           <Image
             source={require('./AuthResources/googlelogo.png')}
@@ -50,6 +101,8 @@ export default class WhoAreYou extends Component {
           />
           <Text style={{ color: '#D33B32' }}>Continue with Google</Text>
         </TouchableOpacity>
+
+        {/* Condition Statement */}
         <Text style={styles.condition}>By tapping the Faebook icon, Google icon, or Signup button, you agree to our{" "}
           <Text onPress={() => this.props.navigation.navigate('TermsAndConditions')} style={{ color: '#3366BB' }}>
             Terms and Conditions
