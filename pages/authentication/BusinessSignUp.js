60eb2eb016747058fa0bb17ce8b8701c70715cec
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-02-28 21:37:28 -0800

    Update

---
+++ pages/authentication/BusinessSignUp.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class BusinessSignUp extends Component {
+  static navigationOptions = {
+    headerStyle: {
+      borderBottomWidth: 0,
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container} >
+        <Text style={styles.title}>Claim your business</Text>
+        <TextInput style={styles.input}
+          placeholder="Store Name"
+        />
+        <TextInput style={styles.input}
+          placeholder="Phone Number"
+        />
+        <TextInput style={styles.input}
+          placeholder="Address"
+        />
+        <TextInput style={styles.input}
+          placeholder="City"
+        />
+        <View style={styles.container2}>
+          <TextInput style={styles.input2}
+            placeholder="State"
+          />
+          <TextInput style={styles.input2}
+            placeholder="Zip"
+          />
+        </View>
+        <TextInput style={styles.input}
+          placeholder="Pick-up Time"
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: 'white' }} onPress={() => this.props.navigation.navigate('BusinessDash')}>Claim</Text>
+
+        </TouchableOpacity>
+        <Text style={styles.condition}>By tapping the Faebook icon, Google icon, or Signup button, you agree to our{" "}
+          <Text onPress={() => this.props.navigation.navigate('TermsAndConditions')} style={{ color: '#3366BB' }}>
+            Terms and Conditions
+          </Text>
+          {" "}and
+          <Text onPress={() => this.props.navigation.navigate('PrivacyStatement')} style={{ color: '#3366BB' }}>
+            {" "}Privacy Statement
+          </Text>
+          .
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    flexDirection: 'row'
+  },
+  input: {
+    height: 40,
+    width: '85%',
+    textAlign: 'right',
+    borderBottomWidth: 1
+  },
+  input2: {
+    height: 40,
+    textAlign: 'right',
+    width: '42.5%',
+    borderBottomWidth: 1
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D33B32',
+    borderRadius: 10,
+    marginTop: 100,
+    padding: 10,
+    width: '85%',
+  },
+  title: {
+    marginBottom: 50,
+    fontSize: 25
+  },
+  condition: {
+    fontSize: 10,
+    width: '85%',
+    paddingTop: 15,
+  }
+});
--- pages/authentication/Signin.js
+++ pages/authentication/Signin.js
@@ -9,7 +9,7 @@ import {
   Button
 } from 'react-native';
 
-export default class Signin extends Component {
+export default class SignIn extends Component {
   static navigationOptions = {
     headerStyle: {
       borderBottomWidth: 0,
@@ -18,26 +18,17 @@ export default class Signin extends Component {
   render() {
     return (
       <View style={styles.container} >
-
-        <View style={styles.containerA}>
-          <Image style={styles.image} source={require('./AuthResources/surplus.jpg')} />
-        </View>
-
-        <View style={styles.form}>
-          <TextInput style={styles.input}
-            placeholder="Email"
-          />
-          <TextInput style={styles.input} secureTextEntry={true}
-            placeholder="Password"
-          />
-        </View>
-
-        <View style={styles.buttons}>
-          <TouchableOpacity style={styles.button}>
-            <Text style={{ color: 'white' }}>Sign In</Text>
-          </TouchableOpacity>
-          <Text style={styles.textButton} onPress={() => this.props.navigation.navigate('Signin')}>Forgot Password?</Text>
-        </View>
+        <Image style={styles.image} source={require('./resources/surplus.jpg')} />
+        <TextInput style={styles.input}
+          placeholder="Email"
+        />
+        <TextInput style={styles.input} secureTextEntry={true}
+          placeholder="Password"
+        />
+        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('CustomerDashboard')}>
+          <Text style={{ color: 'white' }}>Sign In</Text>
+        </TouchableOpacity>
+        <Text style={styles.textButton} onPress={() => this.props.navigation.navigate('SignIn')}>Forgot Password?</Text>
       </View>
     );
   }
@@ -47,55 +38,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
-    // borderWidth: 1,
-    // borderColor: 'blue',
-    margin: 25,
+    justifyContent: 'center',
   },
   input: {
-    // height: 40,
-    paddingTop: 10,
-    paddingBottom: 8,
-    width: '100%',
+    height: 40,
+    width: '85%',
     textAlign: 'right',
     borderBottomWidth: 1
   },
-  form: {
-    width: '100%',
-    // borderWidth: 1,
-    // borderColor: 'orange',
-  },
   button: {
     alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: '#D33B32',
     borderRadius: 10,
-    // marginTop: 200,
-    marginBottom: 10,
-    // marginTop: 15,
-    // padding: 10,
-    height: 45,
-    width: '100%',
-  },
-  buttons: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    // borderWidth: 1,
-    // borderColor: 'orange',
+    marginTop: 200,
+    marginBottom: 15,
+    padding: 10,
+    width: '85%',
   },
   image: {
     height: 60,
     resizeMode: 'contain',
-    alignItems: 'center',
-    justifyContent: 'center',
-    // borderWidth: 1,
-    // borderColor: 'red',
-    marginTop: 30,
-    // marginBottom: 50
+    marginTop: 70,
+    marginBottom: 50
   },
   textButton: {
     color: '#0645AD',
-    // marginTop: 10,
+    marginTop: 10,
   },
 });
