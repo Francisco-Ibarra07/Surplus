d913337fb464d72fd4593f4934b947a6fbf038e0
Jasmine Mai <33132825+jasminemai97@users.noreply.github.com>
2019-03-08 17:16:25 -0800

    Visual Changes for Business Verify

--- pages/authentication/GetStarted.js
+++ pages/authentication/GetStarted.js
@@ -63,7 +63,7 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     flex: 1,
-    padding: '5%',
+    padding: 25,
     justifyContent: 'center',
   },
   containerTop: {
