66e20c50159372270fabaf228c088058540e10e7
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2019-03-22 21:08:35 -0700

    Added CartItem component

---
+++ pages/dashboard/components/CartItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default class CartItem extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleCartItemPress = () => {
+
+  }
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
