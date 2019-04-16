1b0ae5b63a75b781f4618771b5f59910b8f09008
Jasmine Mai <33132825+jasminemai97@users.noreply.github.com>
2019-04-16 14:23:32 -0700

    Update FoodItem Style

--- pages/dashboard/components/FoodItem.js
+++ pages/dashboard/components/FoodItem.js
@@ -17,15 +17,20 @@ export default class FoodItem extends Component {
       <View style={styles.foodItemContainer}>
         <TouchableOpacity style={styles.foodItem}>
 
-          <Text>{this.props.foodItemName}</Text>
-          <View style={styles.foodItemImage}>
-            <Image style={styles.foodItemImageChild} source={{ uri: this.props.foodItemImage }} />
+          <View style={styles.foodName}>
+            <Text style={styles.itemName}>{this.props.foodItemName}</Text>
           </View>
 
-          <View style={styles.foodItemDescription}>
-            <View style={styles.foodTitle}>
-              <Text>Description: {this.props.foodItemDescription}</Text>
-              <Text>Quantity: {this.props.foodItemQuantity}</Text>
+          <View style={styles.desciption}>
+            <View style={styles.foodItemImage}>
+              <Image style={styles.foodItemImageChild} source={{ uri: this.props.foodItemImage }} />
+            </View>
+
+            <View style={styles.foodItemDescription}>
+              <View style={styles.foodTitle}>
+                <Text>Description: {this.props.foodItemDescription}</Text>
+                <Text>Quantity: {this.props.foodItemQuantity}</Text>
+              </View>
             </View>
           </View>
 
@@ -39,41 +44,63 @@ const styles = StyleSheet.create({
   random: {
     color: 'blue',
   },
+  foodName: {
+    padding: 5,
+    // borderWidth: 1,
+    // borderColor: 'red',
+  },
+  itemName: {
+    // fontSize: 16,
+  },
   foodItemContainer: {
     //borderWidth: 1,
-    height: 75,
+    height: 125,
     marginBottom: 20,
+    flex: 1,
   },
   foodItem: {
     borderColor: 'black',
+    borderRadius: 10,
     borderWidth: 1,
 
     marginLeft: 25,
     marginRight: 25,
     // marginBottom: 20,
-    alignItems: 'center',
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
   },
   foodItemImage: {
     // borderWidth: 1,
     height: '100%',
     width: '40%',
-    borderRadius: 10,
-    overflow: 'hidden',
+    // borderRadius: 10,
   },
   foodItemImageChild: {
     height: '100%',
-    width: '100%'
+    resizeMode: 'contain',
+    width: '100%',
+    // borderWidth: 1,
+    // borderColor: 'red',
+    // borderRadius: 10,
+    // marginLeft: 5,
+    overflow: 'hidden',
+    borderRadius: 5,
+  },
+  desciption: {
+    flex: 1,
+    flexDirection: 'row',
+
+    // borderWidth: 1,
+    // borderColor: 'red',
+    margin: 5,
   },
   foodItemDescription: {
     height: '100%',
     // borderWidth: 1,
-    paddingLeft: 10,
-    width: '60%',
+    paddingLeft: 15,
+    // width: '60%',
     flex: 1,
-    // alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
   foodItemStock2: {
     flexDirection: 'row',
