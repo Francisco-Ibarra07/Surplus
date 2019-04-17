9ce38caceb89917aa04c76010c2649e7b0251a47
Jasmine Mai <33132825+jasminemai97@users.noreply.github.com>
2019-04-17 15:56:42 -0700

    Update BusinessMenu.js

--- pages/dashboard/BusinessMenu.js
+++ pages/dashboard/BusinessMenu.js
@@ -3,7 +3,12 @@ import {
   View,
   Text,
   Button,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
+import { CheckBox } from 'react-native-elements';
 
 export default class BusinessMenu extends Component {
   static navigationOptions = {
@@ -15,12 +20,81 @@ export default class BusinessMenu extends Component {
       />
     ),
   };
+
+  constructor() {
+    super();
+
+    this.state = {
+      checked: false
+    };
+  }
+
   render() {
+    const { checked } = this.state;
     return (
-      <View>
-        <Text onPress={() => this.props.navigation.navigate('AddFood')}> Add Item </Text>
-        <Text>Menu</Text>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('AddFood')}>
+          <Image style={styles.addButton}
+            source={require('./resources/plus.png')}
+          />
+        </TouchableOpacity>
+        <View style={styles.title}>
+          <Text>Active Items</Text>
+        </View>
+        {/* <Text onPress={() => this.props.navigation.navigate('AddFood')}>Add Item</Text> */}
+        <ScrollView>
+          <CheckBox
+            checkedIcon='dot-circle-o'
+            uncheckedIcon='circle-o'
+            title='Order #80: Julie Foster'
+            checkedColor='#D33B32'
+            containerStyle={{
+              backgroundColor: 'transparent',
+              borderWidth: 0,
+              borderBottomWidth: 1,
+              borderBottomColor: 'lightgray'
+            }}
+          />
+          <CheckBox
+            checkedIcon='dot-circle-o'
+            uncheckedIcon='circle-o'
+            title='Order #81: Pamela McDonald '
+            checkedColor='#D33B32'
+            containerStyle={{
+              backgroundColor: 'transparent',
+              borderWidth: 0,
+              borderBottomWidth: 1,
+              borderBottomColor: 'lightgray'
+            }}
+          />
+        </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    // borderWidth: 1,
+    // borderColor: 'orange',
+    flex: 1,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  title: {
+    borderBottomWidth: 1,
+    // borderWidth: 1,
+    // borderColor: 'orange',
+    borderColor: 'lightgray',
+    paddingTop: 8,
+    paddingBottom: 8,
+    // marginBottom: 8,
+  },
+  addButton: {
+    height: 40,
+    width: 40,
+    borderWidth: 2,
+    borderRadius: 20,
+    borderColor: 'black'
+  },
+})
