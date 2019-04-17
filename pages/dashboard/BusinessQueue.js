593e82b004f42efca041d08ad19e861ac294c2c1
Jasmine Mai <33132825+jasminemai97@users.noreply.github.com>
2019-04-17 15:25:14 -0700

    Update BusinessQueue Style

--- pages/dashboard/BusinessQueue.js
+++ pages/dashboard/BusinessQueue.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import {
   View,
+  ScrollView,
   Text,
+  StyleSheet,
 } from 'react-native';
+import { CheckBox } from 'react-native-elements'
 
 export default class BusinessQueue extends Component {
   render() {
     return (
-      <View>
-        <Text>Queue</Text>
+      <View style={styles.container}>
+        <View style={styles.title}>
+          <Text>Active Orders</Text>
+        </View>
       </View>
 
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    // borderWidth: 1,
+    // borderColor: 'orange',
+    flex: 1,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  title: {
+    borderBottomWidth: 1,
+    // borderWidth: 1,
+    // borderColor: 'orange',
+    borderColor: 'lightgray',
+    paddingTop: 8,
+    paddingBottom: 8,
+    marginBottom: 8,
+  },
+})
