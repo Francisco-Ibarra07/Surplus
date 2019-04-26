d829347996f6aa9bafe615463939c7c5cca7550c
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2019-04-26 00:41:20 -0700

    Reinstalled ImagePicker library

--- android/app/src/main/java/com/surplus/MainApplication.java
+++ android/app/src/main/java/com/surplus/MainApplication.java
@@ -3,6 +3,7 @@ package com.surplus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new RNFirebasePackage(),
