6147a432541beb244581d53424590d248d9e8750
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2019-04-29 20:11:38 -0700

    Added initial code to edit restaurant info in the settings

--- pages/dashboard/EditRestaurantInfo.js
+++ pages/dashboard/EditRestaurantInfo.js
@@ -6,6 +6,7 @@ import {
   TextInput,
   Button,
   StyleSheet,
+  Image,
 } from 'react-native';
 import RedButton from '../components/RedButton';
 import firebase from 'react-native-firebase'
@@ -14,14 +15,20 @@ export default class EditRestaurantInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
+      storeName: '',
+      address: '',
       phone: '',
-      updatedFirstName: '',
-      updatedLastName: '',
-      updatedEmail: '',
-      updatedPhone: ''
+      zipcode: '',
+      city: '',
+      state: '',
+      logoURL: null,
+      updatedstoreName: '',
+      updatedAddress: '',
+      updatedPhone: '',
+      updatedZipcode: '',
+      updatedCity: '',
+      updatedState: '',
+      updatedlogoURL: null,
     }
   }
 
@@ -34,20 +41,22 @@ export default class EditRestaurantInfo extends Component {
   populateUserInfo = () => {
 
     const user_id = firebase.auth().currentUser.uid;
-    const refToOwnersAccountInfo = firebase.database().ref('/business/owners/' + user_id);
+    const refToOwnersAccountInfo = firebase.database().ref('/business/owners/' + user_id + '/restaurant');
     const activity = this
 
     refToOwnersAccountInfo.on('value', function (snapshot) {
       const snap = snapshot.val();
 
       activity.setState({
-        firstName: snap['first_name'],
-        lastName: snap['last_name'],
-        email: snap['email'],
-        phone: snap['phone_number']
+        storeName: snap['store_name'],
+        address: snap['address'],
+        phone: snap['store_phone'],
+        zipcode: snap['zipcode'],
+        state: snap['state'],
+        city: snap['city'],
+        logoURL: snap['image']
       })
     });
-
   }
 
   componentDidMount() {
@@ -56,51 +65,67 @@ export default class EditRestaurantInfo extends Component {
 
   updateUserInfo = () => {
 
-    const { updatedFirstName, updatedLastName, updatedEmail, updatedPhone } = this.state;
+    const { updatedstoreName, updatedAddress, updatedPhone, updatedZipcode, updatedState, updatedCity } = this.state;
     const user_id = firebase.auth().currentUser.uid;
     const refToOwnersAccountInfo = firebase.database().ref('/business/owners/' + user_id);
 
-    if (updatedFirstName === '' && updatedLastName === '' && updatedEmail === '' && updatedPhone === '') {
+    if (updatedstoreName === '' && updatedAddress === '' && updatedPhone === '' && updatedZipcode === '' && updatedState === '' && updatedCity === '') {
       alert('Make a change to save changes!');
       return;
     }
 
-    if (updatedFirstName !== '') {
+    if (updatedstoreName !== '') {
       refToOwnersAccountInfo.update({
-        'first_name': updatedFirstName
+        'store_name': updatedstoreName
       })
-
-      this.setState({ firstName: updatedFirstName, updatedFirstName: '' });
+      this.setState({ storeName: updatedstoreName, updatedstoreName: '' });
     }
 
-    if (updatedLastName !== '') {
+    if (updatedAddress !== '') {
+      console.log("new address", updatedAddress)
       refToOwnersAccountInfo.update({
-        'last_name': updatedLastName
+        'address': updatedAddress
       })
-
-      this.setState({ lastName: updatedLastName, updatedLastName: '' });
-    }
-
-    if (updatedEmail !== '') {
-      refToOwnersAccountInfo.update({
-        'email': updatedEmail
-      })
-
-      this.setState({ email: updatedEmail, updatedEmail: '' });
+      this.setState({ address: updatedAddress, updatedAddress: '' });
     }
 
     if (updatedPhone !== '') {
       refToOwnersAccountInfo.update({
-        'phone': updatedPhone
+        'store_phone': updatedPhone
       })
-
       this.setState({ phone: updatedPhone, updatedPhone: '' });
+    }
+
+    if (updatedZipcode !== '') {
+      refToOwnersAccountInfo.update({
+        'zipcode': updatedZipcode
+      })
+      this.setState({ zipcode: updatedZipcode, updatedZipcode: '' });
+    }
+
+    if (updatedState !== '') {
+      refToOwnersAccountInfo.update({
+        'state': updatedState
+      })
+      this.setState({ state: updatedState, updatedState: '' });
+    }
+
+    if (updatedCity !== '') {
+      refToOwnersAccountInfo.update({
+        'city': updatedState
+      })
+      this.setState({ city: updatedCity, updatedCity: '' });
     }
 
     alert('Information was updated!');
   }
 
+  handlePhotoUpload = () => {
+
+  }
+
   render() {
+    const { logoURL } = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.h1}>Your Business Information</Text>
@@ -108,37 +133,51 @@ export default class EditRestaurantInfo extends Component {
         <View style={styles.form}>
           <Text style={styles.p}>Store Name</Text>
           <TextInput style={styles.input}
-            onChangeText={updatedFirstName => this.setState({ updatedFirstName })}
-            placeholder={this.state.firstName}
+            onChangeText={updatedstoreName => this.setState({ updatedstoreName })}
+            placeholder={this.state.storeName}
             autoCorrect={false}
-          // autoCapitalize='none'
-          // onChangeText={
-          //   f_name => this.setState({ f_name })
-          // }
           />
 
           <Text style={styles.p}>Phone Number</Text>
           <TextInput style={styles.input}
-            onChangeText={updatedLastName => this.setState({ updatedLastName })}
-            placeholder={this.state.lastName}
+            onChangeText={updatedPhone => this.setState({ updatedPhone })}
+            placeholder={this.state.phone}
             autoCorrect={false}
           />
 
           <Text style={styles.p}>Adress</Text>
           <TextInput style={styles.input}
-            onChangeText={updatedEmail => this.setState({ updatedEmail })}
+            onChangeText={updatedAddress => this.setState({ updatedAddress })}
             keyboardType="email-address"
-            placeholder={this.state.email}
+            placeholder={this.state.address}
             autoCorrect={false}
           />
 
           <Text style={styles.p}>City</Text>
           <TextInput style={styles.input}
-            onChangeText={updatedPhone => this.setState({ updatedPhone })}
+            onChangeText={updatedCity => this.setState({ updatedCity })}
             keyboardType="number-pad"
-            placeholder={this.state.phone}
+            placeholder={this.state.city}
             autoCorrect={false}
           />
+
+          <Text style={styles.p}>Zipcode</Text>
+          <TextInput style={styles.input}
+            onChangeText={updatedZipcode => this.setState({ updatedZipcode })}
+            keyboardType="number-pad"
+            placeholder={this.state.zipcode}
+            autoCorrect={false}
+          />
+
+          {logoURL && (
+            <View style={styles.imageBox}>
+              <Image style={{ width: '100%', height: '100%' }} source={{ uri: logoURL }} />
+            </View>
+          )}
+
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <Button title="Upload new logo" onPress={this.handlePhotoUpload} />
+          </View>
 
           <RedButton
             buttonText='Save Changes'
@@ -184,5 +223,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginBottom: 25,
     marginTop: 10,
+  },
+  imageBox: {
+    height: 75,
+    width: 75,
+    borderWidth: 1,
+    borderColor: 'black',
   },
 })
