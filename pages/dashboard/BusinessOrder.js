9d5c00eca1aab8a5450c3dd99ed6df3af1d2ccda
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2019-05-08 20:22:17 -0700

    Code so far for handling checkbox press (doesnt work tho)

--- pages/dashboard/BusinessOrder.js
+++ pages/dashboard/BusinessOrder.js
@@ -17,12 +17,52 @@ export default class BusinessMenu extends Component {
 
     this.state = {
       solidItems: [],
+      checkBoxComponents: [],
       pendingItemsComponents: [],
       pendingItemsIsEmpty: true,
       fulfilledItemsComponents: [],
+      fulfilledItemsIsEmpty: true,
       checked1: false,
       checked2: false,
     };
+  }
+
+  handleCheckBoxPress = (index) => {
+
+    console.log("Handle Check box press")
+    console.log("All components:", this.state.checkBoxComponents)
+    console.log("Index pressed:", index)
+    console.log("fulfilled section:", this.state.fulfilledItemsComponents)
+    const components = this.state.checkBoxComponents
+
+    let targetComponent = components[index]
+    const bool = targetComponent['props'].checked
+    const name = targetComponent['props'].title
+
+    console.log("Target:", targetComponent)
+
+    let updatedCheckbox = (<CheckBox
+      key={index}
+      title={name}
+      checkedIcon='dot-circle-o'
+      uncheckedIcon='circle-o'
+      checked={!bool}
+      onPress={() => this.handleCheckBoxPress(index)}
+      checkedColor='#D33B32'
+      containerStyle={{
+        backgroundColor: 'transparent',
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderBottomColor: 'lightgray'
+      }}
+    />)
+
+    console.log("Updated:", updatedCheckbox)
+    components[index] = updatedCheckbox
+    components.push(updatedCheckbox)
+    components.push(updatedCheckbox)
+    console.log("new array:", components)
+    this.setState({ fulfilledItemsComponents: components })
   }
 
   populateItems = () => {
@@ -41,8 +81,6 @@ export default class BusinessMenu extends Component {
         return
       }
 
-      console.log("Snap of sold:", snap)
-
       let soldItemTags = []
       for (let tag in snap) {
         soldItemTags.push(tag)
@@ -57,8 +95,7 @@ export default class BusinessMenu extends Component {
 
         nameOfPerson = currentSoldItem.first_name + ' ' + currentSoldItem.last_name
 
-        titleOfComponent = 'Order #' + i.toString() + ': ' + nameOfPerson
-        console.log(titleOfComponent)
+        titleOfComponent = 'Sold Order #' + (i + 1).toString() + ': ' + nameOfPerson
 
         checkBoxComponents.push(
           <CheckBox
@@ -66,6 +103,8 @@ export default class BusinessMenu extends Component {
             title={titleOfComponent}
             checkedIcon='dot-circle-o'
             uncheckedIcon='circle-o'
+            checked={false}
+            onPress={() => activity.handleCheckBoxPress(i)}
             checkedColor='#D33B32'
             containerStyle={{
               backgroundColor: 'transparent',
@@ -77,10 +116,8 @@ export default class BusinessMenu extends Component {
         )
       }
 
-      console.log(checkBoxComponents)
-      activity.setState({ pendingItemsComponents: checkBoxComponents, pendingItemsIsEmpty: false })
+      activity.setState({ checkBoxComponents: checkBoxComponents, pendingItemsComponents: checkBoxComponents, pendingItemsIsEmpty: false })
     })
-
   }
 
   componentDidMount() {
@@ -96,15 +133,13 @@ export default class BusinessMenu extends Component {
           <View style={styles.title}>
             <Text>Pending Items</Text>
           </View>
-          {!this.state.pendingItemsIsEmpty && this.state.pendingItemsComponents}
+          {!this.state.pendingItemsIsEmpty && this.state.checkBoxComponents}
           {this.state.pendingItemsIsEmpty && (<Text>There are currently no pending items</Text>)}
 
-          <CheckBox
+          {/* <CheckBox
             checkedIcon='dot-circle-o'
             uncheckedIcon='circle-o'
-            title='Order #81: Pamela McDonald '
-            // checked={this.state.checked}
-            // onPress={() => this.setState({ checked: !this.state.checked })}
+            title='Sold Order #81: Pamela McDonald '
             checked={this.state.checked2}
             onPress={() => this.setState({ checked2: !this.state.checked2 })}
             checkedColor='#D33B32'
@@ -114,11 +149,13 @@ export default class BusinessMenu extends Component {
               borderBottomWidth: 1,
               borderBottomColor: 'lightgray'
             }}
-          />
+          /> */}
 
           {/* Fulfilled Items */}
           <View style={styles.title2}>
             <Text>Fulfilled Items</Text>
+            {!this.state.fulfilledItemsIsEmpty && this.state.fulfilledItemsComponents}
+            {this.state.fulfilledItemsIsEmpty && (<Text style={{ textAlign: 'center' }}>There are currently no fulfilled items</Text>)}
           </View>
 
         </ScrollView>
